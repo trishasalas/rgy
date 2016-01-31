c18bddeb6d69c1c871de8bb3aeffc1bcafeca29e
trishasalas <trisha@trishasalas.com>
2016-01-31 17:51:10 -0600

    add page templates directory to theme

---
+++ wp-content/themes/rgy-theme/page-templates/page-home.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Name: Home
+ *
+ * @package rgy
+ */
+
+get_header(); ?>
+
+	<section id="masthead" class="masthead">
+
+
+	</section>
+
+	<section class="featured" class="featured">
+
+	</section>
+
+	<section id="grid-nav" class="grid-nav">
+
+	</section>
+
+
+<?php
+get_footer();
